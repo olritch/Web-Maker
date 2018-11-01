809b6cf7f73ee7ed43c790bab89cbab7f8572642
TSN Student <student@ulem.org>
2018-11-01 14:03:45 -0400

    user service ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit
 import { WidgetHeadingComponent } from './components/widget/widget-edit/widget-heading/widget-heading.component';
 import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
 import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import { UserService } from "./services/user.service.client";
 
 @NgModule({
   declarations: [
@@ -40,7 +41,7 @@ import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-y
   ],
   imports: [
     BrowserModule, Routing],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
