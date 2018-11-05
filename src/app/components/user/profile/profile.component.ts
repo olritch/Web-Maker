a01c0e77ac1bf4896d8c50554089325ffa7a11ef
O.G. Enterprise <olritcholritch@gmail.com>
2018-11-05 10:47:30 -0500

    profile page half way done

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service.client';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  constructor(
+    private activatedRoute:ActivatedRoute, 
+    private userService: UserService
+    ) {}
 
-  constructor() { }
-
+  uid: string;
+  user;
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.uid = params["uid"];
+      this.user = this.userService.findUserById(this.uid);
+      console.log(this.user);
+    });
   }
-
 }
