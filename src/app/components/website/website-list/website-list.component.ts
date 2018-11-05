d6e121dc61d14702ffb6f80fd554aaaffa18fa48
TSN Student <student@ulem.org>
2018-11-05 16:00:40 -0500

    finish website-list WEBSITE-NEW next 20min vid

--- src/app/components/website/website-list/website-list.component.ts
+++ src/app/components/website/website-list/website-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsiteService } from 'src/app/services/website.service.client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-website-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-list.component.css']
 })
 export class WebsiteListComponent implements OnInit {
+  websites: any[];
+  uid: string;
 
-  constructor() { }
+  constructor(
+    private websiteService: WebsiteService, 
+    private activatedRoute: ActivatedRoute  
+    ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.uid = params["uid"];
+      this.websites = this.websiteService.findWebsitesByUser(this.uid);
+    });
   }
-
 }
