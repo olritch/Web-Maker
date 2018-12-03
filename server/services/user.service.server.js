7a63a080387a9bdfe798279bc7be376687e21cc2
TSN Student <student@ulem.org>
2018-12-03 16:17:46 -0500

    finish video 4.1 1:05 min

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -10,45 +10,39 @@ module.exports = function(app) {
         // Update User
         app.put("/api/user", updateUser);
 
-        function createUser(req, res) {
+        async function createUser(req, res) {
             var user = req.body;
-            userModel.createUser(user).then((data) => {
-              res.json(data);
-                });
+
+            const data = await userModel.createUser(user);
+            res.json(data);
             }
 
-        function findUserById(req, res) {
+        async function findUserById(req, res) {
             const userId = req.params["uid"];
-            userModel.findUserById(userId).then(
-                (data) => {
-                    res.json(data);
+            const data = await userModel.findUserById(userId);
+            res.json(data);
                 }
-            )
-    }
-
-    function findUser(req, res) {
+         
+    async function findUser(req, res) {
         const username = req.query["username"];
         const password = req.query["password"];
         if(username && password) {
-            userModel.findUserByCredentials(username,password).then(data => {
-            
-                res.json(data);
-            });
+            const data = await userModel.findUserByCredentials(username,password);
+            res.json(data);
             return;
         }
 
         if (username) {
-          userModel.findUserByUsername(username).then( data => {
-              res.json(data);}   );
-            return;
+          const data = await userModel.findUserByUsername(username);
+          res.json(data);
+          return;
         }
     }
 
-    function updateUser(req,res) {
+    async function updateUser(req,res) {
         const user = req.body;
         const uid = user._id;
-        userModel.updateUser(uid, user).then(data => {
-            res.json(data);
-        });
+        const data = await userModel.updateUser(uid, user);
+        res.json(data);
     }
 };
